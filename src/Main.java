a7cf523219620095b9da49fb04cd8bf39eacc009
Junsu Park <pksjmh5295@gmail.com>
2023-11-24 15:16:12 +0900

    [ADD] LinkedList

---
+++ src/Main.java
@@ -0,0 +1,50 @@
+public class Main {
+    public static void main(String[] args) {
+
+        MyLinkedList<Integer> integerMyLinkedList = new MyLinkedList<>();
+
+        integerMyLinkedList.add(1);
+        integerMyLinkedList.add(2);
+        integerMyLinkedList.add(3);
+
+        System.out.println(integerMyLinkedList.get(2));
+
+        // System.out.println(integerMyLinkedList.get(3));
+
+//        Exception in thread "main" java.lang.IndexOutOfBoundsException: Index: 3 is Out of Range
+//        at MyLinkedList.get(MyLinkedList.java:54)
+//        at Main.main(Main.java:12)
+
+        integerMyLinkedList.delete(0);
+
+        MyLinkedList<Integer>.MyIterator myIter = integerMyLinkedList.iterator();
+
+        while (myIter.hasNext()) {
+            System.out.println(myIter.next());
+        }
+
+        System.out.println();
+        // Stack
+        MyStack<String> stringMyStack = new MyStack<>();
+
+        stringMyStack.push("jun");
+        stringMyStack.push("su");
+
+        System.out.println(stringMyStack.pop());
+        System.out.println(stringMyStack.pop());
+        System.out.println(stringMyStack.isEmpty());
+
+
+        System.out.println();
+        // Queue
+        MyQueue<String> queueMyStack = new MyQueue<>();
+
+        queueMyStack.enqueue("jun");
+        queueMyStack.enqueue("su");
+
+        System.out.println(queueMyStack.dequeue());
+        System.out.println(queueMyStack.dequeue());
+        System.out.println(queueMyStack.isEmpty());
+
+    }
+}
